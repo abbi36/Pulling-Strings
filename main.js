259f3390dae7e0fe41224674d6dc0964951d0994
Abbi Engel <abbi.engel33@gmail.com>
2022-02-01 13:41:23 -0600

    pulling strings 2

--- main.js
+++ main.js
@@ -1,17 +1,13 @@
 function main() {
-    const myName = 'Abbi Engel';
-    const mySplit = myName.split(' ');
-    const first = 'Abbi';
-    const last = 'Engel';
-    
-    console.log(myName);
-    console.log(mySplit);
-    console.log(myName.replace('Abbi Engel', 'Abbigail Ilena Engel'));
-    console.log(myName.toUpperCase());
-    console.log(myName.toLowerCase());
-    console.log(mySplit.reverse("").join(" "));
-    console.log(first.slice(0,2));
-    console.log(last.slice(0,3));
-    console.log(myName[0] + myName[5]);
+    const myMessage = "Hello world!";
+    let result = '';
+
+    for (let i = 0; i < myMessage.length; i += 1) {
+        const char = myMessage[i];
+        
+        result += char + " ";
+    }
+
+    console.log(result);
 }
 main();
