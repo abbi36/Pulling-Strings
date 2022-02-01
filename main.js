97b1972613ac8b6231eb97c4284e380d35519585
Abbi Engel <abbi.engel33@gmail.com>
2022-02-01 15:26:22 -0600

    pulling strings 3

--- main.js
+++ main.js
@@ -4,10 +4,16 @@ function main() {
 
     for (let i = 0; i < myMessage.length; i += 1) {
         const char = myMessage[i];
+
+        if((i % 2) == 0) { 
+            result += char.toUpperCase();
+        } else { 
+            result += char.toLowerCase();
+        }
         
-        result += char + " ";
     }
 
     console.log(result);
 }
+
 main();
